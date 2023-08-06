2722a5867c307c4d5cd81d6840380298178a12d5
SantoshYadaw <santoshyadawprl@gmail.com>
2023-08-06 07:51:34 +0000

    Add make dataset py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,81 @@
-# -*- coding: utf-8 -*-
-import click
+import os
+import re
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from tqdm.auto import tqdm
+
+tqdm.pandas()
+
+import pandas as pd
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+
+import torch
+from transformers import DataCollatorForLanguageModeling, DataCollatorWithPadding, GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArguments, AutoConfig
+from datasets import Dataset
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def clean(review):
+    
+    # Convert to lower case
+    review = review.lower()
+    # Remove any numbers
+    review = re.sub('[^a-z A-Z 0-9-]+', '', review)
+    # Remove any stopwords in english
+    review = " ".join([word for word in review.split() if word not in stopwords.words('english')])
+    
+    return review
 
+
+def prepare_dataset_gpt(raw_data_path: str, logging):
+    
+    # Load raw data
+    logging.info(f"Loading raw data from: {raw_data_path}")
+    data = pd.read_csv(DATA_PATH, encoding="ISO-8859-1")
+    data = data.T.reset_index().T.reset_index(drop=True).rename(columns={0: "text"})
+    
+    # Remove duplicates
+    logging.info(f"Preprocessing data")
+    data = data[~data['text'].duplicated()]
+
+    # Clean up text
+    data['text'] = data['text'].progress_apply(clean)
+    data.to_csv(INTERIM_DATA_PATH, index=False)
+
+    # Split randomly
+    data_train, data_val = train_test_split(data, train_size = TRAIN_SIZE, random_state = SEED)
+    len_data_train = len(data_train)
+    len_data_val = len(data_val)
+    logging.info(f'There are {len_data_train} samples for training and {len_data_val} for validation')
+
+    # Save
+    logging.info(f"Saving data")
+    data_train["split"] = "train"
+    data_val["split"] = "val"
+
+    combined_data = pd.concat([data_train, data_val])
+    combined_data.to_csv(SPLIT_DATA_PATH, index=False)
+    logging.info(f"Prepearation of dataset complete. Data saved to {SPLIT_DATA_PATH}")
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    
+    # Setup logger
+    logging.basicConfig(level=logging.INFO)
+    
+    # Constants
+    HOME_PATH = os.getcwd()
+    logging.info(f"HOME_PATH: {HOME_PATH}")
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    DATA_PATH = os.path.join(HOME_PATH,"data", "raw", "task2.csv")
+    logging.info(f"DATA_PATH: {DATA_PATH}")
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    INTERIM_DATA_PATH= os.path.join(HOME_PATH,"data", "interim", "interim_data.csv")
+    logging.info(f"INTERIM_DATA_PATH: {INTERIM_DATA_PATH}")
 
-    main()
+    SPLIT_DATA_PATH = os.path.join(HOME_PATH,"data","processed","split_data.csv")
+    logging.info(f"SPLIT_DATA_PATH: {SPLIT_DATA_PATH}")
+
+    # Data split constants
+    TRAIN_SIZE = 0.9
+    SEED = 2023
+
+    prepare_dataset_gpt(DATA_PATH, logging)
